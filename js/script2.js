56758ccca78c39dc8bad3aecbc9ecec7c5c3314a
Yvon MENSAH <yvonmensah22@gmail.com>
2024-09-12 11:42:37 +0100

    All animations modified

--- js/script2.js
+++ js/script2.js
@@ -1,44 +1,62 @@
 const box = document.getElementById('box');
 
-// 1. Animation de la taille au clic
+// 1. Animation au clic
 box.addEventListener('click', function() {
     anime({
         targets: '#box',
-        width: '500px',
-        height: '350px',
-        duration: 1000, // Durée de l'animation en ms
-        easing: 'easeInOutQuad'
+        translateX: 1000,
+        duration: 4000,
+        direction: 'alternate',
+        easing: 'easeInOutQuad',
+        loop: 1
     });
 });
 
-// 2. Rotation au double clic
+// 2. Animation au double clic
 box.addEventListener('dblclick', function() {
     anime({
         targets: '#box',
-        rotate: '1turn',
-        duration: 1000,
-        easing: 'easeInOutSine'
+        translateX: {
+            value: 1000,
+            duration: 1000
+          },
+          rotate: {
+            value: 360,
+            duration: 2000,
+            easing: 'easeInOutSine'
+          },
+          scale: {
+            value: 1.5,
+            duration: 1800,
+            delay: 1000,
+            easing: 'easeInOutQuart'
+          },
+          delay: 250
     });
 });
 
-// 3. Changement de couleur au survol
+// 3. Changement de taille, de forme et de couleur au survol
 box.addEventListener('mouseover', function() {
     anime({
         targets: '#box',
-        backgroundColor: '#FF6347', // Tomate
-        duration: 1000,
+        width: '500px',
+        height: '400px',
+        backgroundColor: '#FF6347',
+        borderRadius: ['0%', '25%'],
+        duration: 1500,
         easing: 'easeInOutQuad'
     });
 });
 
-// 4. Changer la forme et la couleur quand la souris sort
+// 4. Revenir à l'état initial quand la souris sort
 box.addEventListener('mouseout', function() {
     anime({
         targets: '#box',
-        left: '240px',
-        backgroundColor: '#FFF',
-        borderRadius: ['0%', '50%'],
-        duration: 1000,
+        width: '400px',
+        height: '300px',
+        backgroundColor: '#abd7e6',
+        borderRadius: ['25%', '0%'],
+        duration: 1500,
         easing: 'easeInOutQuad'
     });
 });
@@ -48,14 +66,14 @@ document.addEventListener('keydown', function() {
     anime({
         targets: '#box',
         keyframes: [
-            {translateY: -40},
-            {translateX: 250},
-            {translateY: 40},
+            {translateY: -100},
+            {translateX: 1000},
+            {translateY: 100},
             {translateX: 0},
             {translateY: 0}
           ],
-        duration: 4000,
+        duration: 8000,
         easing: 'easeOutElastic(1, .8)',
-        loop: true
+        loop: 1
     });
 });
